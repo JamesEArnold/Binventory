906b088732ef332a8970ac927e0da1d595b82aff
JamesEArnold <jearnold22@gmail.com>
2025-03-30 21:26:03 -0400

    config(images): add remotePatterns for S3 hosted images

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        hostname: 'localhost',
+        port: '4566',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
